ea4a41cebce987a8625f1dc9743421c60dd70c3c
jajoyce <jjoyce4@gmail.com>
2022-01-22 17:43:08 -0500

    Create array of CRUDable country objects.

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
+const countries = require('./models/countries.js')
+
 app.get('/', (req, res) => {
     res.send('This is the home page. This is working.');
+});
+
+app.get('/countries', (req, res) => {
+    res.send(countries);
 });
 
 app.listen(PORT, () => console.log(`App is running on Port ${PORT}`));
