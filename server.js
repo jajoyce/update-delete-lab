8b13dc6a7f6e01ad5ac19c876851ae76d11a6964
jajoyce <jjoyce4@gmail.com>
2022-01-22 20:24:50 -0500

    Create a Delete route to remove specified object from countries array and redirect to index. Use method-override.

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const app = express();
+const methodOverride = require('method-override');
 const PORT = 4000;
 
 app.use('/static', express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
 
 const countries = require('./models/countries.js');
 
 app.get('/countries', (req, res) => {
     res.render('index.ejs', { countries });
 });
+
+app.delete('/countries/:countryIndex', (req, res) => {
+    countries.splice(req.params.countryIndex, 1);
+    res.redirect('/countries');
+})
 
 app.get('/*', (req, res) => {
     res.redirect('/countries');
